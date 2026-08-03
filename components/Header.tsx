222e4b8a2c2d1caee55d28f90cefb572743f913e
WITT-IT <alexander.witt@witt-itsolutions.de>
2026-08-03 22:03:10 +0200

    Add 'Favoriten' link to navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 const NAV_LINKS = [
+  { href: '/favoriten', label: 'Favoriten'},
   { href: '/organisationen', label: 'Organisationen' },
   { href: '/kontakt', label: 'Kontakt' },
 ];
