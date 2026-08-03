2eed0aa7c7a370d92e9bcc522bf16682aadb9532
WITT-IT <alexander.witt@witt-itsolutions.de>
2026-08-03 22:36:33 +0200

    Add navigation links for organizations and contact

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,8 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 const NAV_LINKS = [
-  
+  { href: '/organisationen', label: 'Für Organisationen' },
+  { href: '/kontakt', label: 'Kontakt' },
 
 ];
 
